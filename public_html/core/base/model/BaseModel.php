ffb3413ca19d76036eaa54f2307c23a45a3fa2d6
nordraw <nikita.tretyak093@gmail.com>
2021-11-08 10:17:04 +0200

    Adding a method "createWhere" in BaseModel

--- public_html/core/base/model/BaseModel.php
+++ public_html/core/base/model/BaseModel.php
@@ -24,6 +24,13 @@ class BaseModel
         $this->db->query("SET NAMES UTF8");
     }
 
+    /**
+     * @param $query
+     * @param string $crud
+     * @param false $return_id
+     * @return array|bool|int|string
+     * @throws DbException
+     */
     final public function query($query, $crud = 'r', $return_id = false)
     {
 
@@ -69,7 +76,7 @@ class BaseModel
      * @param $table <p>Таблица базы данных</p>
      * @param array $set
      * <p>'fields' => ['fio', 'name']</p>
-     * <p>'where' => ['fio' => 'Smirnova', 'name' => 'Masha', 'surname' => 'Ivanova']</p>
+     * <p>'where' => ['surname' => 'Smirnova', 'name' => 'Masha', 'patronymic' => 'Ivanova']</p>
      * <p>'operand' => ['=', '<>']</p>
      * <p>'condition' => ['AND']</p>
      * <p>'order' => ['fio', 'name']</p>
@@ -77,21 +84,25 @@ class BaseModel
      * <p>'limit' => '1'</p>
      * @return array|bool|int|string
      * @throws DbException
+     * '= <> IN NOT IN LIKE (SELECT * FROM ......)'
      */
     final public function get($table, $set = [])
     {
         $fields = $this->createFields($table, $set);
+
         $where = $this->createWhere($table, $set);
 
+        exit(var_dump($where));
+
         $join_arr = $this->createJoin($table, $set);
+
+        $order = $this->createOrder($table, $set);
 
         $fields .= $join_arr['fields'];
         $join = $join_arr['join'];
         $where .= $join_arr['where'];
 
         $fields = rtrim($fields, ',');
-
-        $order = $this->createOrder($table, $set);
 
         $limit = $set['limit'] ? $set['limit'] : '';
 
@@ -107,10 +118,11 @@ class BaseModel
      */
     protected function createFields($table = false, $set)
     {
+        //Проверяем, пришла ли таблица для формирования запроса вида: "SELECT * FROM table.value....."
+        $table = $table ? $table . '.' : '';
+
         $set['fields'] = (is_array($set['fields']) && !empty($set['fields']))
             ? $set['fields'] : ['*'];
-
-        $table = $table ? $table . '.' : '';
 
         $fields = '';
 
@@ -154,5 +166,106 @@ class BaseModel
         }
 
         return $order_by;
+    }
+
+    /**
+     * @param false $table
+     * @param $set
+     * @param string $instruction
+     * @return false|string|void
+     *
+     * <p> Пример обрабатываемых данных:</p>
+     * <p>'where' => ['surname' => 'Smirnova, Ivanova', 'name' => 'Masha', 'patronymic' => 'Ivanovna']</p>
+     * <p>'operand' => ['IN', 'LIKE%', '<>']</p>
+     * <p>'condition' => ['AND']</p>
+     */
+    protected function createWhere($table = false, $set, $instruction = 'WHERE ')
+    {
+        $table = $table ? $table . '.' : '';
+
+        $where = '';
+
+        if (is_array($set['where']) && !empty($set['where'])) {
+            //Операнд по-умолчанию "="
+            $set['operand'] = (is_array($set['operand']) && !empty($set['operand']))
+                ? $set['operand'] : ['='];
+
+            //Условие по-умолчанию "AND"
+            $set['condition'] = (is_array($set['condition']) && !empty($set['condition']))
+                ? $set['condition'] : ['AND'];
+
+            $where = $instruction;
+
+            $operand_count = 0;
+            $condition_count = 0;
+
+            foreach ($set['where'] as $key => $item) {
+                $where .= ' ';
+
+                //Разбираем, какой операнд пришёл
+                if ($set['operand'][$operand_count]) {
+                    $operand = $set['operand'][$operand_count];
+                    $operand_count++;
+                } else {
+                    $operand = $set['operand'][$operand_count - 1];
+                }
+
+                //Разбираем условие (AND, OR)
+                if ($set['condition'][$condition_count]) {
+                    $condition = $set['condition'][$condition_count];
+                    $condition_count++;
+                } else {
+                    $condition = $set['condition'][$condition_count - 1];
+                }
+
+                if ($operand === 'IN' || $operand === 'NOT IN') {
+                    if (is_string($item) && strpos($item, 'SELECT')) {
+                        $in_str = $item;
+                    } else {
+                        if (is_array($item)) $temp_item = $item;
+                        else $temp_item = explode(',', $item);
+
+                        $in_str = '';
+                        foreach ($temp_item as $value) {
+                            $in_str .= "'" . trim($value) . "',";
+                        }
+                    }
+
+                    $where .= $table . $key . ' ' . $operand . ' (' . rtrim($in_str, ',') . ') ' . $condition;
+
+                }
+                //Знаки % приходят в ячейку operand. Например, "%LIKE%".
+                //Их необходимо переместить в $item.
+                //Таким образом формируем запрос вида: 'SELECT * FROM table WHERE name LIKE "%Ivan%"'
+                elseif (strpos($operand, 'LIKE') !== false) {
+                    $like_template = explode('%', $operand);
+
+                    foreach ($like_template as $lt_key => $lt) {
+                        if (!$lt) {
+                            if (!$lt_key) {
+                                $item = '%' . $item;
+                            } else {
+                                $item .= '%';
+                            }
+                        }
+                    }
+
+                    $where .= $table . $key . ' LIKE ' . "'" . $item . "' $condition";
+                } //Если приходят обычные операторы, например "=", "<>"
+                else {
+                    //Если в $item лежит вложенный запрос
+                    if (strpos($item, 'SELECT') === 0) {
+                        $where .= $table . $key . $operand . ' (' . $item . ") $condition";
+                    } else {
+                        $where .= $table . $key . $operand . "'" . $item . "' $condition";
+                    }
+                }
+            }
+
+            //Обрезаем последнее условие
+            $where = substr($where, 0, strrpos($where, $condition));
+
+            return $where;
+        }
     }
 }
